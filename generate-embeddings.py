0816369a0741030ec34d621c29bed9a5d63268bf
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2023-06-22 17:22:04 +0200

    code summarization + embedding generation

--- generate-embeddings.py
+++ generate-embeddings.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 from openai.embeddings_utils import get_embedding
-from utils import CodeReader
+from utils import CodeReader, CodeSummarizer
 import pandas as pd
 import numpy as np
 import openai
@@ -38,18 +38,34 @@ def main():
 
     files = [y for x in os.walk(directory) for ext in extensions for y in glob.glob(os.path.join(x[0], '*'+ext))]
 
+    code_data = []
     for file_path in files:
+        in_context_file_path = file_path.replace(f'{directory}/', '')
+
         reader = CodeReader(file_path)
         code = reader.get_full_code()
+        code = f'# file: {in_context_file_path}\n{code}'
+        code_embedding = get_embedding(code, engine=EMBEDDING_MODEL)
+
+        code_summarization = CodeSummarizer.summarize(code, model='gpt-4')
+        code_summarization_embedding = get_embedding(code_summarization, engine=EMBEDDING_MODEL)
 
         data_item = {
             'filename': os.path.basename(file_path),
-            'filepath': file_path,
-            'code': code
+            'filepath': in_context_file_path,
+            'code': code,
+            'code_embedding': code_embedding,
+            'code_summarization': code_summarization,
+            'code_summarization_embedding': code_summarization_embedding
         }
+        code_data.append(data_item)
 
-        exit()
+        print(f'processed {file_path}')
 
+    df = pd.DataFrame(code_data)
+    df.to_csv("api_embedding_data.csv", index=False)
+
+    print(f'\n[info] embeddings generated for {len(code_data)} files.')
 
 if __name__ == "__main__":
     main()
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,2 @@
 from utils.code_reader import CodeReader
+from utils.code_summarizer import CodeSummarizer
