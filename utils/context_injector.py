20f417e020b67ab52f89f9c126a97b9c5349734a
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2023-06-23 11:03:04 +0200

    context injector - wip...

--- utils/context_injector.py
+++ utils/context_injector.py
@@ -7,6 +7,9 @@ EMBEDDING_MODEL = "text-embedding-ada-002"
 
 
 class ContextInjector:
+    """
+    Class responsible for retrieving and injecting context most relevant to user's prompt.
+    """
 
     def __init__(self, context_file: str = None):
         self.__context_file = 'api_embedding_data.csv'
@@ -18,8 +21,15 @@ class ContextInjector:
         self.__df['code_summarization_embedding'] = self.__df['code_summarization_embedding'].apply(lambda x: json.loads(x))
 
     def get_context_for_prompt(self, prompt_query: str, max_context_items=3):
+        """
+        Search and retrieve x most relevant pieces of context.
+        """
+
         query_embedding = get_embedding(prompt_query, engine=EMBEDDING_MODEL)
         # self.__df['similarities'] = self.__df['code_embedding'].apply(lambda x: cosine_similarity(x, query_embedding))
         self.__df['similarities'] = self.__df['code_summarization_embedding'].apply(lambda x: cosine_similarity(x, query_embedding))
 
         return self.__df.sort_values('similarities', ascending=False).head(max_context_items)
+
+
+    # todo: measure relevancy to better classify context items (data)
