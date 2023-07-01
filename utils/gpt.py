b19de96231ec304c1abf5f2bfae37f4de976a39f
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2023-07-01 13:14:35 +0200

    gpt client - add functions option for later use

--- utils/gpt.py
+++ utils/gpt.py
@@ -8,11 +8,16 @@ openai.api_key = os.environ.get('OPENAI_API_KEY')
 class GPTClient:
 
     @staticmethod
-    def prompt_gpt_api(messages: list, model='gpt-3.5-turbo-16k', temperature=0.0):
-        response = openai.ChatCompletion.create(
-            model=model,
-            temperature=temperature,
-            messages=messages
-        )
+    def prompt_gpt_api(messages: list, model='gpt-3.5-turbo-16k', temperature=0.0, functions=None):
+        args = {
+            'model': model,
+            'temperature': temperature,
+            'messages': messages
+        }
+
+        if functions is not None:
+            args['functions'] = functions
+        
+        response = openai.ChatCompletion.create(**args)
 
         return response['choices'][0]['message']['content']
