31989f223c2c32a26e6eb7caca0083f2f177e7e8
Ibragim Abubakarov <ibragim.ai95@gmail.com>
2023-07-04 21:33:58 +0200

    run agent script improvement
    prompt user for input if `prompt.txt` file not found.

--- run_agent.py
+++ run_agent.py
@@ -1,5 +1,12 @@
 from agents.code_agent import CodeAgent
+import os
 
+
+if not os.path.isfile('prompt.txt'):
+    prompt = input('What should i do on linux-tower ?\n')
+
+    with open('prompt.txt', 'w') as f:
+        f.write(prompt)
 
 with open('prompt.txt', 'r') as f:
     prompt = f.read().strip()
